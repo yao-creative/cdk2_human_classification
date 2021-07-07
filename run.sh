07136e11f0c2df0f0e20aa7b57251ba1e8f6d0d7
Yi Yao Tan <yao@Yis-MacBook-Pro.local>
2021-07-07 21:45:02 +0200

    fixed run.sh

--- run.sh
+++ run.sh
@@ -2,18 +2,20 @@
 
 cd PDB_files
 ls *.pdb
-run=#?
-cd ../
-if [ $run -eq None ]; then
+get_pdbs=$?
+#validity is 0 if there are pdb
+echo "validity $get_pdbs"
+if [ $get_pdbs != 0 ] ; then
     echo "No pdb files, downloading pdb files"
-    curl https://www.uniprot.org/uniprot/P63208.txt > human_cdk2_uniprot.txt
+    curl https://www.uniprot.org/uniprot/P24941.txt > human_cdk2_uniprot.txt
     python3 pdb_codes.py
     python3 pdb_get.py
     cd PDB_files
     ./batch_download.sh -f searchlist.txt -p
     python3 uncompress.py
-    cd ../
+    
 fi
+cd ../
+echo "annotating"
 python3 annotate.py
 open annotated.txt
-
