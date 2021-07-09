dfecce9c1b36a461f40d7af556fb93c3d2e5e690
Yi Yao Tan <yao@Yis-MacBook-Pro.local>
2021-07-09 21:42:47 +0200

    I did something 2

--- structures/pymol_list.py
+++ structures/pymol_list.py
@@ -1,13 +1,28 @@
 import pickle
 import os
-outfile = open("pdbs2.txt", "w")
+import sys
+sys.path.insert(1, "../")
+import helper
+pymol_list = open("pymol_list.txt", "w")
+opened_active = open("opened_active.txt", "w")
+closed_inactive = open("closed_active.txt", "w")
+opened_inactive = open("opened_inactive.txt", "w")
+
 os.chdir("..")
-with open("pdbs.var", "rb") as f:
+with open("annotated.var", "rb") as f:
     pdb_dict = pickle.load(f)
 
 
-for pdb in pdb_dict:
-    outfile.write(pdb + " ")
 
-outfile.close()
+for pdb in pdb_dict:
+    pdb =pdb_dict[pdb]
+    pymol_list.write(pdb.code + " ")
+    if "open" in pdb.group:
+        if "active" in pdb.group:
+            opened_active.write(pdb.code + " ")
+        else:
+            opened_inactive.write(pdb.code + " ")
+    else:
+        closed_inactive.write(pdb.code + " ")
+pymol_list.close()
 f.close()
